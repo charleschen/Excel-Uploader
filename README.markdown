d98f0241943d525f9daf384f9b1ff7bb0cb39860
Charles Chen <ccchen920@gmail.com>
2011-12-07 11:58:53 -0800

    first commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+#   Excel Uploader
+
+
+an example of using carrierwave to upload an excel spreadsheet to Amazon S3.  Has the ability to read the excel spreadsheet too.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uploader::Application.config.secret_token = '1eef665b962f3611079c5639965171572202277ea1bcecb5ae093d05e8d62b21af8a49c33d054e686284c19840ad17585d941918972e437cb74858394d7977d8'
